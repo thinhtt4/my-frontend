169033767219411019f0f02ab24bfd152913b6a6
thinhtt4 <tienthinhtt4@gmail.com>
2025-12-25 09:48:56 +0700

    fix CORS

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState("")
 
   useEffect(() => {
-    fetch("https://my-backend-ppre.onrender.com/api/hello")
+    fetch("https://my-backend-ppne.onrender.com/api/hello")
       .then(res => res.json())
       .then(data => setMessage(data.message))
   }, [])
